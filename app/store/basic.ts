6990abe782e9abd38601c13207b13d866f650e66
Dennis Karlsson <tougent@gmail.com>
2023-05-03 15:29:09 +0200

    fix: address

--- app/store/basic.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-interface ZustandStore {
-    base_url: string,
-}
-
-export const basicStore = create<ZustandStore>(() => ({
-    base_url: "https://pokedex.onrender.com:3004"
-}))
